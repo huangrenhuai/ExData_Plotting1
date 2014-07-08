1724791d6e8c356d7d1b117032d5a721d4b76652
Ren-Huai Huang <huangrenhuai@google.com>
2014-07-08 13:40:13 -0500

    1. finish plot 1

---
+++ plotting.R
@@ -0,0 +1,39 @@
+### plotting.R
+
+
+### step1: Loading data.
+setwd("./") # set to an empty working directory. change accordingly. 
+
+### download and unzip the data
+getData <- function(){
+   if (!file.exists("temp.zip")){
+      url <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+      download.file(url, destfile = "temp.zip", method = "curl")
+      unzip("temp.zip")}
+}
+
+### load the data
+loadData <- function(){
+   message("loading a table with a size of 20 Mb")
+   file1 <- "./household_power_consumption.txt" 
+   myData <<- read.csv2(file1, stringsAsFactors = FALSE, na.strings = "?")
+   myData$Date <<- as.Date(myData$Date, format = "%d/%m/%Y")
+   myDatab <- subset(myData, Date >= as.Date("2007-02-01"))
+   myDatab <- subset(myData, Date <= as.Date("2007-02-02"))
+   
+
+}
+
+### Plot 1
+plot1 <- function(){
+
+hist(as.numeric(myDatab$Global_active_power), col="red", xlab = "Global Active Power (kilowatts)")
+}
+
+### Plot 2
+
+
+### plot 3
+
+
+### plot 4
